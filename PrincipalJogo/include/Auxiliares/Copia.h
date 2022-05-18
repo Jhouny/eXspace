55072b66d2d494f44ba5fa52c8b2e3f685055fd9
robertoluiz26 <robertoluiz@alunos.utfpr.edu.br>
2022-05-18 17:52:16 -0300

    classe lista e copia templates

---
+++ PrincipalJogo/include/Auxiliares/Copia.h
@@ -0,0 +1,11 @@
+#ifndef _COPIA_H_
+#define _COPIA_H_
+template<class CE> class Copia{
+    private:
+        Copia<CE>*pProx;
+    public:
+        set
+};
+
+
+#endif
---
+++ PrincipalJogo/include/Auxiliares/Lista.h
@@ -0,0 +1,10 @@
+#ifndef _LISTA_H_
+#define _LISTA_H_
+
+template <class LT> class Lista{
+    private:
+
+    
+    public:
+};
+#endif
